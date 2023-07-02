d34cac1f79615987331543db3c2996072de0b3cb
Sergy007 <kutsenkosergey@mail.ru>
2023-07-02 13:24:06 +0300

    В этой версии скрипта использован os.path для создания относительного пути на основе текущего расположения скрипта. Пути к файлам данных были преобразованы в относительные пути, которые будут работать, если файлы данных находятся в папке north_data, расположенной в одной директории со скриптом.

--- homework-1/main.py
+++ homework-1/main.py
@@ -1,5 +1,13 @@
 """Скрипт для заполнения данными таблиц в БД Postgres."""
+import os
 import psycopg2
+
+# Определение пути к файлам данных относительно расположения скрипта
+current_dir = os.path.dirname(os.path.abspath(__file__))
+customers_data_path = os.path.join(current_dir, "north_data", "customers_data.csv")
+employees_data_path = os.path.join(current_dir, "north_data", "employees_data.csv")
+orders_data_path = os.path.join(current_dir, "north_data", "orders_data.csv")
+
 
 conn = psycopg2.connect(
     host="localhost",
@@ -10,21 +18,24 @@ conn = psycopg2.connect(
 cur = conn.cursor()
 
 """заполнение таблиц данными"""
-with open("C:/Users/Sergy007/PycharmProjects/postgres-homeworks/homework-1/north_data/customers_data.csv", 'r') as file:
+with open(customers_data_path, 'r') as file:
+#with open("C:/Users/Sergy007/PycharmProjects/postgres-homeworks/homework-1/north_data/customers_data.csv", 'r') as file:
     next(file)
     for line in file:
         data = line.strip().split(',')
         cur.execute("INSERT INTO customers (customer_id, company_name, contact_name) VALUES (%s, %s, %s) ON CONFLICT DO NOTHING",
                     (data[0], data[1], data[2]))
 
-with open("C:/Users/Sergy007/PycharmProjects/postgres-homeworks/homework-1/north_data/employees_data.csv", 'r') as file:
+with open(employees_data_path, 'r') as file:
+#with open("C:/Users/Sergy007/PycharmProjects/postgres-homeworks/homework-1/north_data/employees_data.csv", 'r') as file:
     next(file)
     for line in file:
         data = line.strip().split(',')
         cur.execute("INSERT INTO employees (employee_id, first_name, last_name, title, birth_date, notes) VALUES "
             "(%s, %s, %s, %s, %s, %s) ON CONFLICT DO NOTHING", (data[0], data[1], data[2], data[3], data[4], data[5]))
 
-with open("C:/Users/Sergy007/PycharmProjects/postgres-homeworks/homework-1/north_data/orders_data.csv", 'r') as file:
+with open(orders_data_path, 'r') as file:
+#with open("C:/Users/Sergy007/PycharmProjects/postgres-homeworks/homework-1/north_data/orders_data.csv", 'r') as file:
     next(file)
     for line in file:
         data = line.strip().split(',')
